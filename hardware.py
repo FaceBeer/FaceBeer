1ed4f6ee3f40fc7545008ecb834604441d010a91
Emre <emresbz@gmail.com>
2023-01-12 18:45:09 -0500

    more minor fixes

--- hardware.py
+++ hardware.py
@@ -20,10 +20,16 @@ class Display:
         self.DC = 23
         self.SPI_PORT = 0
         self.SPI_DEVICE = 0
-        self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)# 128x64 display with hardware I2C:
+        self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=self.RST)# 128x64 display with hardware I2C:
         # Initialize library.
         self.disp.begin()
         self.clear_display()
+        self.width = self.disp.width
+        self.height = self.disp.height
+        self.image = Image.new('1', (self.width, self.height))
+        self.draw = ImageDraw.Draw(self.image)
+        #load Font
+        self.font = ImageFont.truetype('PixelOperator8.ttf',16)
 
         
     def clear_display(self):
@@ -32,21 +38,13 @@ class Display:
         self.disp.display()
 
     def write_display(self,msg):
-        width = self.disp.width
-        height = self.disp.height
-        self.image = Image.new('1', (width, height))
-        self.draw = ImageDraw.Draw(self.image)
-        #load Font
-        self.font = ImageFont.truetype('PixelOperator8.ttf',16)
         # Draw a black filled box to clear the image.
-        self.draw.rectangle((0,0,width,height), outline=0, fill=0)
+        self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)
         #write in the middle of display
-        self.draw.text((30, 23), str(msg),font=font, fill=255)
+        self.draw.text((30, 23), str(msg),font=self.font, fill=255)
         self.disp.image(self.image)
         self.disp.display()
         time.sleep(.1)
 
-    def breathalyzer_read(self):
-        self.mq3reading = self.adc.read_adc(0, gain=self.GAIN)
-        return self.mq3reading
+    
 
