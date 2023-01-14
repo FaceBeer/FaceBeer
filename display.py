57b6f00c34ff722e400993d3f517d40ac6eb9810
Emre <emresbz@gmail.com>
2023-01-13 21:05:29 -0500

    test display

--- display.py
+++ display.py
@@ -21,19 +21,12 @@ oled_reset = digitalio.DigitalInOut(board.D4)
 # Change these
 # to the right size for your display!
 WIDTH = 128
-HEIGHT = 32  # Change to 64 if needed
+HEIGHT = 64 
 BORDER = 5
 
 # Use for I2C.
 i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
-
-# Use for SPI
-# spi = board.SPI()
-# oled_cs = digitalio.DigitalInOut(board.D5)
-# oled_dc = digitalio.DigitalInOut(board.D6)
-# oled = adafruit_ssd1306.SSD1306_SPI(WIDTH, HEIGHT, spi, oled_dc, oled_reset, oled_cs)
 
 # Clear display.
 oled.fill(0)
