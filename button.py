480248b9fb6c60570e175a620d8d4267a374b1cc
GrantPerkins <gcperk20@gmail.com>
2023-01-13 22:18:58 -0500

    Begin state machine

--- button.py
+++ button.py
@@ -1,9 +1,2 @@
-import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-buttonPin = 16
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-while True:
-    buttonState = GPIO.input(buttonPin)
-    if buttonState == False:
-        print("Pushed")
+
