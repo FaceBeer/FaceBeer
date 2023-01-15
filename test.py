5fcfd80b412725e58d80521e0574044142e03fcd
GrantPerkins <gcperk20@gmail.com>
2023-01-14 20:08:21 -0500

    Log test

--- test.py
+++ test.py
@@ -6,7 +6,8 @@ model = Model()
 preds = []
 truths = []
 for name in ["grant", "connor", "max", "emre"]:
-    for i in range(100):
+    print(name)
+    for i in range(50):
         path = f"/home/facebeer/data/data/{name}/{str(i).zfill(5)}.jpg"
         img = Image.open(path)
         pred, _ = model.predict(img)
