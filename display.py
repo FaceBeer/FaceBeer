6b3f7cb40657a1f0df16abfb8c2bca1381762e81
GrantPerkins <gcperk20@gmail.com>
2023-01-14 19:11:15 -0500

    Minor refactoring

--- display.py
+++ display.py
@@ -12,22 +12,19 @@ class Display:
         oled_reset = digitalio.DigitalInOut(board.D4)
         self.BORDER = 5
         self.WIDTH = 128
-        self.HEIGHT = 64 
-        #For I2C
+        self.HEIGHT = 64
+        # For I2C
         self.i2c = board.I2C()  # uses board.SCL and board.SDA
         self.oled = adafruit_ssd1306.SSD1306_I2C(self.WIDTH, self.HEIGHT, self.i2c, addr=0x3C, reset=oled_reset)
-        
-        
+        self.font = ImageFont.truetype('PixelOperator8.ttf', 20)
+        self.velocity = -10
 
     def clear_display(self):
         # Clear display.
         self.oled.fill(0)
         self.oled.show()
-        
 
-    def display_write(self,msg):
-        # load Font
-        self.font = ImageFont.truetype('PixelOperator8.ttf', 20)
+    def display_write(self, text):
         # Create blank image for drawing.
         # Make sure to create image with mode '1' for 1-bit color.
         image = Image.new("1", (self.oled.width, self.oled.height))
@@ -36,46 +33,38 @@ class Display:
         # Draw a white background
         draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=255, fill=255)
         # Draw a smaller inner rectangle
-        draw.rectangle((self.BORDER, self.BORDER, self.oled.width - self.BORDER - 1, self.oled.height - self.BORDER - 1),outline=0,fill=0,)
-        # Draw Some Text
-        text = msg
+        draw.rectangle(
+            (self.BORDER, self.BORDER, self.oled.width - self.BORDER - 1, self.oled.height - self.BORDER - 1),
+            outline=0, fill=0)
         maxwidth, unused = draw.textsize(text, font=self.font)
         (font_width, font_height) = self.font.getsize(text)
-        #set scroll speed
-        velocity = -10
-        pos = self.oled.width
+        position = self.oled.width
         if font_width > self.oled.width:
             while True:
-                draw.rectangle((0,0,self.oled.width,self.oled.height), outline=0, fill=0)
-                x = pos
+                draw.rectangle((0, 0, self.oled.width, self.oled.height), outline=0, fill=0)
+                x = position
                 for i, c in enumerate(text):
-                # Stop drawing if off the right side of screen.
+                    # Stop drawing if off the right side of screen.
                     if x > self.oled.width:
                         break
-                # Calculate width but skip drawing if off the left side of screen.
+                    # Calculate width but skip drawing if off the left side of screen.
                     if x < -10:
                         char_width, char_height = draw.textsize(c, font=self.font)
                         x += char_width
                         continue
                     # Draw text.
-                    draw.text((x, self.oled.height// 2- font_height//2), c, font=self.font, fill=255)
+                    draw.text((x, self.oled.height // 2 - font_height // 2), c, font=self.font, fill=255)
                     # Increment x position based on chacacter width.
                     char_width, char_height = draw.textsize(c, font=self.font)
                     x += char_width
                 # Draw the image buffer.
                 self.oled.image(image)
                 self.oled.show()
-                pos += velocity
-                if pos < -maxwidth:
-                    pos = self.oled.width
+                position += self.velocity
+                if position < -maxwidth:
+                    position = self.oled.width
         elif font_width < self.oled.width:
-            draw.text((self.oled.width // 2 - font_width // 2, self.oled.height // 2 - font_height // 2),text,font=self.font,fill=255,)
+            draw.text((self.oled.width // 2 - font_width // 2, self.oled.height // 2 - font_height // 2), text,
+                      font=self.font, fill=255, )
             self.oled.image(image)
             self.oled.show()
-           
-
-
-
-
-
-
